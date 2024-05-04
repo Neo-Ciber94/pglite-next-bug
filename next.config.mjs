70528a82722e97631365192ff41589e15c160687
Freddy L <neo-ciber_94@hotmail.com>
2024-05-04 15:03:47 -0400

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, options) => {
+    if (!options.isServer) {
+      config.resolve.fallback = { fs: false, module: false, path: false };
+    }
+
+    return config;
+  },
+  transpilePackages: ["@electric-sql/pglite"],
+};
 
 export default nextConfig;
